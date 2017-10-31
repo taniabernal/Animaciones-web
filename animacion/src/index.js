7a4c9264abd1307b782b809e8a5c6ec24ecb3236
Tania Bernal <tanibu18@gmail.com>
2017-10-30 18:59:38 -0600

    Animaciones Invie

--- animacion/src/index.js
+++ animacion/src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Invie from './App';
+import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<Invie />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
